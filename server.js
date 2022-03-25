d950e5678b6fddf0b20a395e36c3533e38fd5d78
trulstp <trulstp@stud.ntnu.no>
2022-03-25 13:51:44 +0100

    testng

--- server.js
+++ server.js
@@ -9,9 +9,14 @@ const PORT = process.env.PORT || 5000
 
 dotenv.config()
 
-mongoose.connect(process.env.DATABASE_ACCESS, () =>console.log("Database connected"))
-app.use(express.static(path.resolve(__dirname, "../src/index.js")));
+mongoose.connect(
+    process.env.DATABASE_ACCESS, {
+    useNewUrlParser: true, 
+    useUnifiedTopology:true,
+})
+.then(() =>console.log("Database connected"))
+.catch((err) => console.log(err))
 app.use(express.json())
 app.use(cors())
 app.use('/app', routesUrls)
-app.listen(PORT, () => console.log("server is up and running fine")) 
+app.listen(PORT, () => console.log(`server is up and running fine on ${PORT}`)) 
